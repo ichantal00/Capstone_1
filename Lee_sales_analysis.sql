c43cc678c335f4e195e0a8ca89e8b1bbc74681c3
Chantal.Lee <leechantal83@gmail.com>
2026-04-30 05:34:59 -0400

    updated lee_sales and pushed excel

--- Lee_sales_analysis.sql
+++ Lee_sales_analysis.sql
@@ -1,7 +1,18 @@
--- What is total revenue overall for sales in the assigned territory, plus the start date and end date
+-- Questions -- ==============================================================
+-- 1.What is total revenue overall for sales in the assigned territory, plus the start date and end date
 -- that tell you what period the data covers
 
+-- 2.What is the month by month revenue breakdown for the sales territory?
 
+-- 3.Provide a comparison of total revenue for the specific sales territory and the region it belongs to.
+
+-- 4.What is the number of transactions per month and average transaction size by product category 
+-- for the sales territory?
+
+-- 5.Can you provide a ranking of in-store sales performance by each store in the sales territory, 
+-- or a ranking of online sales performance by state within an online sales territory?
+
+-- 6.What is your recommendation for where to focus sales attention in the next quarter?
 
 USE sample_sales;
 -- =================================================================
@@ -33,7 +44,7 @@ INNER JOIN store_locations
 ON store_sales.store_id = store_locations.storeid
 INNER JOIN store_managers
 ON store_managers.state = store_locations.state
-GROUP BY store_manager; -- $144
+GROUP BY store_manager; -- 
 
 -- =====================================================================
 -- -- CONNECTICUT -- --
@@ -53,6 +64,7 @@ INNER JOIN store_locations
 ON store_sales.store_id = store_locations.storeid
 WHERE state = 'Connecticut'; -- total = '2,392,222.44', average = '137.66'
 
+
 -- ================================================================
 -- -- NEW YORK -- -- 
 
@@ -71,6 +83,43 @@ FROM store_sales
 INNER JOIN store_locations
 ON store_sales.store_id = store_locations.storeid
 WHERE state = 'New York'; -- total = '4,330,817.09', average = '139.53'
+
+-- ======================================================================
+
+-- IN STORE --
+-- Month by Month -- 
+
+-- Connecticut --
+SELECT DATE_FORMAT(Transaction_Date, '%Y-%m') AS month,
+    ROUND(SUM(sale_amount), 2) AS total_revenue
+FROM store_sales
+INNER JOIN store_locations
+ON store_sales.Store_ID = store_locations.StoreID
+WHERE state = 'Connecticut'
+GROUP BY month
+ORDER BY month;
+
+-- New York --
+SELECT DATE_FORMAT(Transaction_Date, '%Y-%m') AS month,
+    ROUND(SUM(sale_amount), 2) AS total_revenue
+FROM store_sales
+INNER JOIN store_locations
+ON store_sales.Store_ID = store_locations.StoreID
+WHERE state = 'New York'
+GROUP BY month
+ORDER BY month;
+
+-- -- General East -- --
+SELECT DATE_FORMAT(Transaction_Date, '%Y-%m') AS month,
+    ROUND(SUM(sale_amount), 2) AS total_revenue
+FROM store_sales
+INNER JOIN store_locations
+ON store_sales.Store_ID = store_locations.StoreID
+INNER JOIN store_managers
+ON store_locations.state = store_managers.State
+WHERE region = 'East'
+GROUP BY month
+ORDER BY month;
 
 -- ===============================================================
 -- Online sales by month and years
@@ -176,3 +225,15 @@ JOIN online_sales
 ON products.ProdNum = online_sales.ProdNum
 WHERE shiptostate = 'New York'
 Group by product;
+
+
+
+
+
+
+
+
+
+
+
+
